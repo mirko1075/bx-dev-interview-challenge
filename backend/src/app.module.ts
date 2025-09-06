2c8a6dcc3edd918638aa54713a25718358da0641
Mirko Siddi <mirko.siddi@gmail.com>
2025-09-06 19:07:01 +0200

    feat: setup database connection and define user entity

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,30 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import getCommonConfig from './configs/common';
 import { AppController } from './controllers/app.controller';
 import { AppService } from './services/app/app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, load: [getCommonConfig] })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [getCommonConfig] }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('database.host'),
+        port: configService.get<number>('database.port'),
+        username: configService.get<string>('database.user'),
+        password: configService.get<string>('database.password'),
+        database: configService.get<string>('database.name'),
+        entities: [User],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/entities/user.entity.ts
@@ -0,0 +1,25 @@
+import {
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Entity,
+} from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password?: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+}
